64e3188104f976b3bd53b41a6063f2a62cbe90ec
olku11 <artem.chastukhin@gmail.com>
2023-02-13 20:31:46 +0300

    5

---
+++ 5.py
@@ -0,0 +1,40 @@
+import sys
+from io import BytesIO
+# Этот класс поможет нам сделать картинку из потока байт
+import requests
+from PIL import Image
+import function
+import disance
+import pygame
+
+toponym_to_find = " ".join(sys.argv[1:])
+
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+if not response:
+    pass
+json_response = response.json()
+finx, finy = function.coord(json_response)
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+toponym_coodrinates = toponym["Point"]["pos"]
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": ",".join([toponym_coodrinates.split(" ")]),
+    'kind': 'district',
+    'results': '1',
+    'format': 'json'
+}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+print(response.json()["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]['name'])
