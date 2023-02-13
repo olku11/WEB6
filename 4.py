205e09fdc8ff65e276c050cfa31e672573d7cd85
olku11 <artem.chastukhin@gmail.com>
2023-02-13 20:26:01 +0300

    4_2

--- 4.py
+++ 4.py
@@ -18,7 +18,7 @@ geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
 geocoder_params = {
     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": "ул.Новаторов, д.36к2",
+    "geocode": toponym_to_find,
     "format": "json"}
 
 response = requests.get(geocoder_api_server, params=geocoder_params)
